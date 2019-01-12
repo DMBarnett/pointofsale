2c8250ae1073aa376de0f046383ec4948e53c516
DMBarnettLaptop <davidmitchellbarnett@gmail.com>
2019-01-12 11:59:17 -0500

    Working on login

--- client/src/screens/Footer/index.js
+++ client/src/screens/Footer/index.js
@@ -1,24 +1,22 @@
 import React from "react";
-import { Container, Row, Col } from "reactstrap";
+import { Row, Col } from "reactstrap";
 import "./style.css";
 
 function Footer(){
   return(
     <div className="footer-fixed">
-      <Container >
-        <Row>
-          <Col size="md-3 sm-6">
-          <h3>DMBarnett</h3>
-          </Col>
-          <Col size="md-3 sm-6">
-          <h3>Point of Sale</h3>
-          </Col>
-          <Col size="md-3 sm-6">
-          </Col>
-          <Col size="md-3 sm-6">
-          </Col>
-        </Row>
-      </Container>
+      <Row>
+        <Col md="3" sm="6">
+        <h5>A DMBarnett website </h5>
+        </Col>
+        <Col md="3" sm="6">
+        <h3>Point of Sale</h3>
+        </Col>
+        <Col md="3" sm="6">
+        </Col>
+        <Col md="3" sm="6">
+        </Col>
+      </Row>
     </div>
   )
 }
--- client/src/screens/Header/index.js
+++ client/src/screens/Header/index.js
@@ -1,11 +1,20 @@
 import React from "react";
 import { Navbar, Nav, NavItem, NavLink, NavbarBrand } from "reactstrap"
+import "./style.css"
 
 function Header(){
   return(
     <div>
       <Navbar>
         <NavbarBrand href="/">Home</NavbarBrand>
+        <Nav className="ml-auto" navbar>
+          <NavItem>
+            <NavLink href="/users">Users</NavLink>
+          </NavItem>
+          <NavItem>
+            <NavLink href="/inventory">Inventory</NavLink>
+          </NavItem>
+        </Nav>
       </Navbar>
     </div>
   )
