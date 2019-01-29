29537e505f75c4d9df7b5777cceb13e4ce200a3f
DMBarnettLaptop <davidmitchellbarnett@gmail.com>
2019-01-29 17:53:40 -0500

    heroku

--- models/index.js
+++ models/index.js
@@ -3,7 +3,7 @@
 const fs = require("fs");
 const path = require("path");
 const Sequelize = require("sequelize");
-const env = process.env.NODE_ENV || "development";
+const env = process.env.JAWS_DB || "development";
 const config = require(__dirname +"/../config/config.json")[env];
 const sequelize = new Sequelize(config);
 let db = {};
